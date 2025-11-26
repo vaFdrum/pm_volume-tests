3164405abbd549c007371a44a06d1d845733d8ca
vaFdrum <va.fdrum@gmail.com>
2025-11-26 15:17:07 +0300

    Update managers.py

--- common/managers.py
+++ common/managers.py
@@ -29,6 +29,11 @@ class UserPool:
 
 
 class StopManager:
+    """
+    Менеджер для контроля выполнения:
+    - Отслеживает сколько пользователей завершили все свои итерации
+    - Останавливает тест когда ВСЕ пользователи завершили
+    """
     _instance = None
     _lock = threading.Lock()
     _initialized = False
@@ -43,24 +48,51 @@ class StopManager:
     def __init__(self):
         if not StopManager._initialized:
             self._lock = threading.Lock()
+            self._iterations_per_user = CONFIG.get("max_iterations", 1)
+            self._total_users = 1
+            self._completed_users = 0
+            self._user_iterations = {}
             self._should_stop = False
-            self._completed_iterations = 0
-            self._max_iterations = 1
             self._stop_called = False
             StopManager._initialized = True
 
-    def set_max_iterations(self, max_iterations):
+    def setup_scenario(self, total_users):
+        """Настройка сценария при запуске теста"""
         with self._lock:
-            self._max_iterations = max_iterations
+            if total_users is None:
+                total_users = 1
 
-    def increment_iteration(self):
+            self._total_users = total_users
+            self._completed_users = 0
+            self._user_iterations = {}
+            self._should_stop = False
+            self._stop_called = False
+            self._iterations_per_user = CONFIG.get("max_iterations", 1)
+
+    def user_completed_iteration(self, user_id):
+        """
+        Пользователь завершил одну итерацию
+        Возвращает: (user_finished, global_stop)
+        """
         with self._lock:
-            self._completed_iterations += 1
-            if self._completed_iterations >= self._max_iterations:
+            if user_id not in self._user_iterations:
+                self._user_iterations[user_id] = 0
+
+            self._user_iterations[user_id] += 1
+
+            user_finished = self._user_iterations[user_id] >= self._iterations_per_user
+
+            if user_finished:
+                self._completed_users += 1
+
+            global_stop = self._completed_users >= self._total_users
+
+            if global_stop and not self._should_stop:
                 self._should_stop = True
-            return self._should_stop
+            return user_finished, global_stop
 
     def should_stop(self):
+        """Должен ли весь тест остановиться"""
         with self._lock:
             return self._should_stop and not self._stop_called
 
@@ -74,11 +106,16 @@ class StopManager:
 
     def get_stats(self):
         with self._lock:
+            total_iterations = sum(self._user_iterations.values())
             return {
-                "completed": self._completed_iterations,
-                "max": self._max_iterations,
+                "completed_users": self._completed_users,
+                "total_users": self._total_users,
+                "iterations_per_user": self._iterations_per_user,
+                "total_iterations": total_iterations,
+                "user_iterations": dict(self._user_iterations),
                 "should_stop": self._should_stop,
                 "stop_called": self._stop_called
             }
+
 
 stop_manager = StopManager()
